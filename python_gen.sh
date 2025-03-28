55b24a9800b906a5cd8d70f0bc7f8fd73cf1937f
jstzwj <1103870790@qq.com>
2025-03-28 23:28:34 +0800

    update proto

--- python_gen.sh
+++ python_gen.sh
@@ -1,1 +1,8 @@
-flatc --python -o outputs/python -I Protocols Protocols/*.fbs
+./flatc --python -o outputs/python Protocols/Common.fbs
+./flatc --python -o outputs/python Protocols/Auth.fbs
+./flatc --python -o outputs/python Protocols/Contact.fbs
+./flatc --python -o outputs/python Protocols/Image.fbs
+./flatc --python -o outputs/python Protocols/Register.fbs
+./flatc --python -o outputs/python Protocols/Chat/Common.fbs
+./flatc --python -o outputs/python Protocols/Chat/PrivateChat.fbs
+./flatc --python -o outputs/python Protocols/Chat/GroupChat.fbs
